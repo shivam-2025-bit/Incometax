52258fba25f2b1abf51262099a198d455be9bd9a
shivam-2025-bit <casanjay12@gmail.com>
2026-04-24 15:38:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -472,4 +472,5 @@ if __name__ == "__main__":
     print("  http://127.0.0.1:5000")
     print("  Admin Dashboard: http://127.0.0.1:5000/admin")
     print("=" * 50)
-    app.run(debug=True, port=5000)
+    if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
